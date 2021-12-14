99a0a96fdc79c3ca377fb5de31574e2122b7a457
vophucson <vophucson@gmail.com>
2021-12-14 10:47:50 +0700

    fix

--- src/main/java/vn/cloud/config/Config.java
+++ src/main/java/vn/cloud/config/Config.java
@@ -6,6 +6,6 @@ public class Config {
 	public static String ipServer3 = "172.31.31.74"; // thay đôi ip bằng ip public ec2 thứ 3
 	public static String userSql = "sa"; // thay đổi tài khoản đăng nhập sql container
 	public static String pasSql = "vophucson17110360"; // thay đổi mật khẩu đăng nhập sql container
-	public static String privatekeyPath = "/home/ubuntu/vophucson_aws.pem"; // thay đổi path của key ec2
+	public static String privatekeyPath = "/usr/local/tomcat/webapps/vophucson_aws.pem"; // thay đổi path của key ec2
 	public static String databaseName = "usercloud";
 }

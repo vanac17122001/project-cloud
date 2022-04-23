0a5859aa03d48ee09a87b3abc16126a2f17cdf66
Cao Anh Văn <84551062+vanac17122001@users.noreply.github.com>
2022-04-23 15:24:34 +0700

    Update Config.java

--- src/main/java/vn/cloud/config/Config.java
+++ src/main/java/vn/cloud/config/Config.java
@@ -1,11 +1,11 @@
 package vn.cloud.config;
 
 public class Config {
-	public static  String ipServer1 = "172.31.23.162"; // thay đổi ip bằng ip public trên ec2 chứa database
-	public static String ipServer2 = "172.31.94.35"; // thay đổi ip bằng ip public ec2 thứ 2
-	public static String ipServer3 = "172.31.31.74"; // thay đôi ip bằng ip public ec2 thứ 3
+	public static  String ipServer1 = "172.31.9.188"; // thay đổi ip bằng ip public trên ec2 chứa database
+	public static String ipServer2 = "172.31.3.196"; // thay đổi ip bằng ip public ec2 thứ 2
+	public static String ipServer3 = "172.31.3.93"; // thay đôi ip bằng ip public ec2 thứ 3
 	public static String userSql = "sa"; // thay đổi tài khoản đăng nhập sql container
-	public static String pasSql = "vophucson17110360"; // thay đổi mật khẩu đăng nhập sql container
-	public static String privatekeyPath = "/usr/local/tomcat/webapps/vophucson_aws.pem"; // thay đổi path của key ec2
-	public static String databaseName = "usercloud";
+	public static String pasSql = "caoanhvan@19133067"; // thay đổi mật khẩu đăng nhập sql container
+	public static String privatekeyPath = "/home/ubuntu/key-cloud--main/DoAn_Docker_Ubuntu.pem"; // thay đổi path của key ec2
+	public static String databaseName = "MyDb0";
 }
